d8b027227cf681aad7a89968ce336f6e675c955b
root <marcolovecchio0102@gmail.com>
2024-11-27 16:27:32 +0000

    ros workspace init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/assignment1/src
+# Build directory: /root/assignment1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
